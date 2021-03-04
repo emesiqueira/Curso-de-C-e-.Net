fa408b34a561260cbea60a344628b82f32e3f7ea
Emerson Siqueira <emersonsiqueira@live.com>
2021-03-03 23:04:12 -0300

    Primeiro programa

---
+++ MeuCursoCSharp/Fundamentos/PrimeiroPrograma.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MeuCursoCSharp.Fundamentos
+{
+    class PrimeiroPrograma
+    {
+        public static void Executar()
+        {
+            Console.WriteLine("Primeiro ");
+            Console.WriteLine("Programa");
+            Console.WriteLine("Terminou!");
+
+        }
+    }
+}
